b81eef1fcda240587e13e832ff75b7606155eea3
CavanOrBxj <40481651+CavanOrBxj@users.noreply.github.com>
2018-08-13 09:00:16 +0800

    重庆江津项目对接建仓

---
+++ trunk/GRPlatForm/DeviceInfoReport.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GRPlatForm
+{
+    [Serializable]
+    public class DeviceInfoReport
+    {
+        public string RPTStartTime;
+
+        public string RPTEndTime;
+
+        public Device Device;
+    }
+
+    public class Device
+    {
+        public string SRV_ID;
+        public string DeviceID;
+
+        public string DeviceCategory;
+
+        public string DeviceType;
+
+        public string DeviceName;
+
+        public string AreaCode;
+
+        public string AdminLevel;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /*-------------2018-06-10添加-----------------*/
+        public string Srv_Mft_Date;
+        public string UpdateDate;
+        public string Old_Srv_Mft_Date;
+        public string Old_UpdateDate;
+        /*------------------------------*/
+        /// <summary>
+        /// 
+        /// </summary>
+        public string DeviceState;
+        /// <summary>
+        /// 经度
+        /// </summary>
+        public string Longitude;
+        /// <summary>
+        /// 纬度
+        /// </summary>
+        public string Latitude;
+    }
+
+}
---
+++ trunk/GRPlatForm/SingletonInfo.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Collections.Generic;
+using System.Data;
+
+namespace GRPlatForm
+{
+    public class SingletonInfo
+    {
+        private static SingletonInfo _singleton;
+
+
+        /// <summary>
+        /// 经度
+        /// </summary>
+        public string Longitude;
+        /// <summary>
+        /// 纬度
+        /// </summary>
+        public string Latitude;
+
+
+        public string PlayRecordLastTime;
+
+        public string TsCmd_ValueID_;
+
+        public string sZJPostUrlAddress;
+        public string TerminalLastTime;
+        private SingletonInfo()                                                                 
+        {
+            Longitude = "";
+            Latitude = "";
+            PlayRecordLastTime = "";
+
+            //终端
+            TerminalLastTime = "";
+            TsCmd_ValueID_ = "";
+            sZJPostUrlAddress = "";
+        }
+        public static SingletonInfo GetInstance()
+        {
+            if (_singleton == null)
+            {
+                Interlocked.CompareExchange(ref _singleton, new SingletonInfo(), null);
+            }
+            return _singleton;
+        }
+    }
+}
